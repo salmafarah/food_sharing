f54c5e14582d695cd7d17c386bec9f82ec78989b
Salma Farah <salmafarah08@gmail.com>
2020-04-06 22:49:15 -0400

    update model'
    \q
    exit
    git
    
    exit()
    \q

--- food_sharing/main_app/models.py
+++ food_sharing/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    exp = models.CharField(max_length=10)
+    count = models.CharField(max_length=10)
+    image = models.ImageField
+    location = models.CharField(max_length=100)
+    contact_name = models.CharField(max_length=100)
+    tele = models.CharField(max_length=100)
+
+
+
+
+
--- food_sharing/main_app/views.py
+++ food_sharing/main_app/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Food
 
 # Create your views here.
 
-class Food: 
-    def __init__(self,name,description, exp, count, image, location, contact_name, tele):
-        self.name = name
-        self.description = description
-        self.exp = exp
-        self.count = count
-        self.image = image
-        self.location = location
-        self.contact_name = contact_name
-        self.tele = tele
+# class Food: 
+#     def __init__(self,name,description, exp, count, image, location, contact_name, tele):
+#         self.name = name
+#         self.description = description
+#         self.exp = exp
+#         self.count = count
+#         self.image = image
+#         self.location = location
+#         self.contact_name = contact_name
+#         self.tele = tele
 
-food = [
-    Food('Canned lentil soup', 'I have 1 canned soup available', '2029/01/11', '1 can', 'https://imgur.com/CEUW2yL.jpg', 'Toronto, Ontario', 'James', '416-435-6578'),
-    Food('Ramen noddle', 'Chicken and Beef ramen noddles', '2024/12/30', '12 pak', 'https://imgur.com/ywy11IY.jpg', 'Ottawa, Ontario', 'Lul','647-982-3721' ),
-    Food('Linguine Pasta', "I can give away 2 packs of pasta", '2025/05/14','2pak', 'https://imgur.com/GbjIIa9.jpg', 'Waterloo, Ontario', 'Ayan', '647-223-5496'),
-    Food('Chips', 'Pack of chips', '2030/07/25','12 pak', 'https://imgur.com/jli7RNb.jpg','Toronto, Ontario', 'Sarah', '416-123-0976')
-]
+# foods = [
+#     Food('Canned lentil soup', 'I have 1 canned soup available', '2029/01/11', '1 can', 'https://imgur.com/CEUW2yL.jpg', 'Toronto, Ontario', 'James', '416-435-6578'),
+#     Food('Ramen noddle', 'Chicken and Beef ramen noddles', '2024/12/30', '12 pak', 'https://imgur.com/ywy11IY.jpg', 'Ottawa, Ontario', 'Lul','647-982-3721' ),
+#     Food('Linguine Pasta', "I can give away 2 packs of pasta", '2025/05/14','2pak', 'https://imgur.com/GbjIIa9.jpg', 'Waterloo, Ontario', 'Ayan', '647-223-5496'),
+#     Food('Chips', 'Pack of chips', '2030/07/25','12 pak', 'https://imgur.com/jli7RNb.jpg','Toronto, Ontario', 'Sarah', '416-123-0976')
+# ]
 
 
 
@@ -27,4 +28,5 @@ def home(request):
     return render(request,'home.html')
 
 def shares(request):
-    return render(request, 'index.html')
+    foods = Food.objects.all()
+    return render(request, 'index.html', {'foods':foods})
