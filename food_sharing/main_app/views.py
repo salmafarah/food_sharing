d72284b9df921c2e8c57bb0859683e32d0937369
Salma Farah <salmafarah08@gmail.com>
2020-04-10 18:36:56 -0400

    updated photo in view

--- food_sharing/main_app/views.py
+++ food_sharing/main_app/views.py
@@ -2,7 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Food
+from .models import Food, Comments, Photo
 from .forms import CommentsForm
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3.us-west-1.amazonaws.com/'
+BUCKET = 'sharewfriends'
+
 
 # Create your views here.
 
@@ -57,4 +64,18 @@ def add_comment(request,food_id):
         new_comment.food_id = food_id
         new_comment.save()
     return redirect('show_one', food_id=food_id)
+
+def add_photo(request, food_id):
+    photo_file=request.FILES.get('photo-file', None)
+    if photo_file: 
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try: 
+            s3.upload_fileobj(photo_file,BUCKET,key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Photo(url=url,food_id=food_id)
+            photo.save()
+        except: 
+            print('An error ocurred uploding an image')
+        return redirect ('show_one', food_id=food_id)
 
