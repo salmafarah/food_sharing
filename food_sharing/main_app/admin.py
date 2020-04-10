bf57966e8b9b2a70831ed567e0c26a4828a969ce
Salma Farah <salmafarah08@gmail.com>
2020-04-09 21:48:10 -0400

    made comment model

--- food_sharing/main_app/admin.py
+++ food_sharing/main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Food 
+from .models import Food, Comments 
 
 # Register your models here.
 admin.site.register(Food)
+admin.site.register(Comments)
 
